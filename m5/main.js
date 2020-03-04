835557d1d70f73d23f5c1a39f24f0d0009a3b88c
Hillel Feldman <17299316+x1p0d@users.noreply.github.com>
2020-03-04 15:27:45 +0200

    milestone 5

---
+++ m5/main.js
@@ -0,0 +1,19 @@
+function calculateFibonacci(index) {
+    fetch("http://localhost:5050/fibonacci/" + index)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            result.innerText = data.result;
+        });
+}
+
+function changeToFibonacci() {
+    let x = document.getElementById("user-input").value;
+    result.innerText = calculateFibonacci(x);
+}
+
+let result = document.getElementById('result');
+document.getElementById("get-input-button").addEventListener("click", changeToFibonacci);
+
+for (let i = 0; i < 10; i++); {
+    console.log(true);
+}
